d19fc7dfc7fb09df03fc0365ce508c995b12a5f9
Zathura160801 <adipz160801@gmail.com>
2024-12-23 06:19:00 +0700

    setup cms

--- app/Models/Testimonial.php
+++ app/Models/Testimonial.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Testimonial extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'boarding_house_id',
         'photo',
+        'name',
         'content',
         'rating',
     ];
